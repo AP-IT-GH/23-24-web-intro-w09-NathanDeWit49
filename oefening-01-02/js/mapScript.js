e9c10d20f1e991f959ce3913b532191a7d382df2
NathanDeWit49 <nadjandewit@gmail.com>
2024-04-23 12:56:59 +0200

    klaar

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -2,9 +2,18 @@
 * Gebruik hiervoor de documentatie op https://leafletjs.com/ 
 * Verander in de code online alle "var" in "let".
 */
+let map = L.map('apMap').setView([51.23009, 4.41616], 17);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
+let marker = L.marker([51.23009, 4.41616]).addTo(map);
 
+let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
+
+L.rectangle(bounds, {color: "#e90000", weight: 1}).addTo(map);
 
 
 
